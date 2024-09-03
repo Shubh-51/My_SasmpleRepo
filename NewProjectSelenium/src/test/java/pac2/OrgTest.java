df02790d4d617eb8819aa63859d473bbaa3c3375
shubh <shubh@Shubh_51.mshome.net>
2024-09-03 12:47:15 +0530

    in org test mvn parameter

--- NewProjectSelenium/src/test/java/pac2/OrgTest.java
+++ NewProjectSelenium/src/test/java/pac2/OrgTest.java
@@ -7,6 +7,15 @@ public class OrgTest {
 	@Test
 	public void createOrg() {
 		System.out.println("CreateOrg");
+		String browser=System.getProperty("browser");
+		String url=System.getProperty("url");
+		String username=System.getProperty("username");
+		String password=System.getProperty("password");
+		
+		System.out.println(browser);
+		System.out.println(url);
+		System.out.println(username);
+		System.out.println(password);
 		
 	}
 	@Test
