d452a61316364e5557896d68997dbce8014d0149
shubh <shubh@Shubh_51.mshome.net>
2024-09-03 11:05:25 +0530

    My First Push to Sample

---
+++ NewProjectSelenium/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,21 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrg() {
+		System.out.println("CreateOrg");
+		
+	}
+	@Test
+	public void modifyOrg() {
+		System.out.println("modifyOrg");
+	}
+	@Test
+	public void deleteOrg() {
+		System.out.println("deleteOrg");
+	}
+
+}
